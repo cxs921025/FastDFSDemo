43128ed2a80be90522310eacba15599bd854636c
cxs <cxs@ronhe.com>
2018-11-29 10:36:04 +0800

    增加 基本fastdfs上传功能

--- src/main/java/com/demo/fastdfs/FastdfsApplication.java
+++ src/main/java/com/demo/fastdfs/FastdfsApplication.java
@@ -1,9 +1,16 @@
 package com.demo.fastdfs;
 
+import com.github.tobato.fastdfs.FdfsClientConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableMBeanExport;
+import org.springframework.context.annotation.Import;
+import org.springframework.jmx.support.RegistrationPolicy;
 
 @SpringBootApplication
+@Import(FdfsClientConfig.class)
+// 防止 com.github.tobato.fastdfs.conn:type=FdfsConnectionPoolfdfsPool 多次注入
+@EnableMBeanExport(registration = RegistrationPolicy.IGNORE_EXISTING)
 public class FastdfsApplication {
 
     public static void main(String[] args) {
